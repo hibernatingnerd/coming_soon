a650138e33be084b782cd980d7aded02242664a4
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-09-11 12:27:56 -0400

    version 2.0 of the site.

---
+++ styles/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+
+  $('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+
+    return false;
+    });
+
+
+
+})
